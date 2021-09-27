7941ebf992a1a274d09b8682f264edca0885aab6
Alex Peuchert <aaalex2021@ki2s.com>
2021-09-27 16:07:56 +0100

    Accessing TFT displays with ILI9488 or SSD1963 controllers via i8080 interface on STM32F2s.

---
+++ klippy/extras/display/ssd1963.py
@@ -0,0 +1,86 @@
+# Support for SSD1963 TFT display controllers
+#
+# Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+import time
+from . import textframebuffer
+from . import i8080overFSMC
+
+# The SSD1963 is a display controller with a 1215K byte frame buffer and
+# supports up to 864 x 480 x 24bit graphics content. It also equips a parallel
+# MCU interface in different bus width to receive graphics data and command
+# from the MCU.
+
+class SSD1963(textframebuffer.TextFrameBuffer):
+    def __init__(self, config):
+        self.width = config.getint('lcd_width', 480, minval=0, maxval=864)
+        self.height = config.getint('lcd_height', 272, minval=0, maxval=480)
+        self.columns = config.getint('columns', 20, minval=16)
+        self.rows = config.getint('rows', 4, minval=4)
+        self.fgcolor = 0xFFFF
+        self.bgcolor = 0x0000
+        self.io = i8080overFSMC.I8080overFSMC(config)
+        textframebuffer.TextFrameBuffer.__init__(self, self.io,
+                                                 self.columns, self.rows,
+                                                 self.width, self.height,
+                                                 self.fgcolor, self.bgcolor)
+    def init(self):
+        #REG(0xE2);   // Set PLL
+        #i8080_send_cmd_param8 oid=1 cmd=226 param=170454
+        self.io.send_cmd(0xE2,[0x17,0x04,0x54])
+        #REG(0xE0);   // Start PLL command
+        #i8080_send_cmd_param8 oid=1 cmd=224 param=01
+        self.io.send_cmd(0xE0,[0x01])
+        time.sleep(0.01)
+        #REG(0xE0);   // Start PLL command again
+        #i8080_send_cmd_param8 oid=1 cmd=224 param=03
+        self.io.send_cmd(0xE0,[0x03])
+        time.sleep(0.01)
+        #REG(0x01);   // Soft reset
+        #i8080_send_cmd oid=1 cmd=1
+        self.io.send_cmd(0x01)
+        time.sleep(0.1)
+        # hard coded TFT50 settings
+        #REG(0xE6);   // 12Mhz
+        #i8080_send_cmd_param8 oid=1 cmd=230 param=013332
+        self.io.send_cmd(0xE6,[0x01, 0x33, 0x32])
+        # hard coded TFT50 parameters 480x272 resolution
+        #REG(0xB0);   // Set LCD mode
+        #i8080_send_cmd_param8 oid=1 cmd=176 param=000001DF010F00
+        self.io.send_cmd(0xB0,[0,0,1,0xDF,1,0x0F,0])
+        #REG(0xB4);   // Set horizontal period
+        #i8080_send_cmd_param8 oid=1 cmd=180 param=020A002900000000
+        self.io.send_cmd(0xB4,[2,0x0A,0,0x29,0,0,0,0])
+        #REG(0xB6);   // Set vertical period
+        #i8080_send_cmd_param8 oid=1 cmd=182 param=011B000A010000
+        self.io.send_cmd(0xB6,[1,0x1B,0,0x0A,1,0,0])
+        #REG(0xF0);   // Set pixel data interface format
+        #i8080_send_cmd_param8 oid=1 cmd=240 param=03
+        self.io.send_cmd(0xF0,[3])
+        #REG(0xBC);   // postprocessor for contrast/brightness/saturation.
+        #i8080_send_cmd_param8 oid=1 cmd=188 param=40804001
+        self.io.send_cmd(0xBC,[0x40,0x80,0x40,1])
+        #REG(0x29);   // Set display on
+        #i8080_send_cmd oid=1 cmd=41
+        self.io.send_cmd(0x29)
+        #REG(0x36);   // Set address mode
+        #i8080_send_cmd_param8 oid=1 cmd=54 param=00
+        self.io.send_cmd(0x36,[0])
+        self.clear_all()
+
+
+    def clear_all(self):
+        ex= self.width - 1
+        self.send_cmd(0x2A,[0,0,ex>>8,ex&0xFF])
+        ey= self.height - 1
+        self.send_cmd(0x2B,[0,0,ey>>8,ey&0xFF])
+        self.send_cmd(0x2C)
+        self.send_fill(0x0000, self.width * self.height)
+
+    def _fill_into_region(self, x_from, x_to, y_from, y_to, words):
+        self.send_cmd(0x2A,[x_from>>8, x_from&0xFF, x_to>>8, x_to&0xFF])
+        self.send_cmd(0x2B,[y_from>>8, y_from&0xFF, y_to>>8, y_to&0xFF])
+        self.send_cmd(0x2C)
+        for d in words: self.send_data(d)
---
+++ klippy/extras/display/textframebuffer.py
@@ -0,0 +1,85 @@
+# TextFrameBuffer class provides a character display on top of a 16 bit
+# graphics display. It uses an IO adapter to send the graphics t0 the display
+#
+# Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+import logging
+from . import font16x24
+
+CHAR_HEIGHT = 24
+CHAR_WIDTH = 16
+
+class TextFrameBuffer:
+    def __init__(self, io, columns=16, rows=4, screen_width=0, screen_height=0,
+                 fgcolor=0xFFFF, bgcolor=0x0000):
+        self.send_cmd = io.send_cmd
+        self.send_data = io.send_data
+        self.send_fill = io.send_fill
+        self.columns = columns
+        self.rows = rows
+        self.x_offset = (screen_width - CHAR_WIDTH*self.columns) /2
+        self.y_offset = (screen_height - CHAR_HEIGHT*self.rows) /2
+        self.fgcolor = [fgcolor>>8, fgcolor & 0xFF]
+        self.bgcolor = [bgcolor>>8, bgcolor & 0xFF]
+        self.vram = [[0x00] * self.columns for i in range(self.rows)]
+        self.font = [self._swizzle_bits(bytearray(c))
+                     for c in font16x24.VGA_FONT_16x24]
+        self.icons = {}
+        self.tbuf_old = [['~' for j in range(self.columns)]
+                         for i in range(self.rows)]
+        self.clear()
+
+    def get_dimensions(self):
+        return (self.columns, self.rows)
+    def set_glyphs(self, glyphs):
+        for glyph_name, glyph_data in glyphs.items():
+            icon = glyph_data.get('icon16x16')
+            if icon is not None:
+                icon_words = [ 0, 0, 0, 0, 0, 0, 0, 0]
+                for ba in zip(icon[0],icon[1]):
+                    icon_words = icon_words + [ba[1], ba[0]]
+                icon_words = icon_words + [ 0, 0, 0, 0, 0, 0, 0, 0]
+                self.icons[glyph_name] = self._swizzle_bits(icon_words)
+    def _swizzle_bits(self, data):
+        c = data
+        c_out = []
+        for i in range(len(c)/2):
+            c_row = c.pop(0) | c.pop(0)<<8
+            ar = []
+            for b in format(c_row,'016b'):
+                ar = ar + (self.fgcolor if b=='1' else self.bgcolor)
+            c_out.append(ar);
+        return [ bytearray(row) for row in c_out ]
+    def clear(self):
+        self.tbuf = [[' ' for j in range(self.columns)]
+                     for i in range(self.rows)]
+    def flush(self):
+        for new_row, old_row, cy in zip(self.tbuf, self.tbuf_old,
+                                        [self.y_offset+y*CHAR_HEIGHT
+                                         for y in range(self.rows)]):
+            for ch, old_ch, cx in zip(new_row, old_row,
+                                      [self.x_offset+x*CHAR_WIDTH
+                                       for x in range(self.columns)]):
+                if ch == old_ch: continue
+                if len(ch) != 1:
+                    chx = self.icons.get(ch)
+                else:
+                    chx = self.font[ord(ch)]
+                self._fill_into_region(cx, cx+(CHAR_WIDTH-1),
+                                       cy, cy+(CHAR_HEIGHT-1),
+                                       chx)
+        self.tbuf_old = self.tbuf
+    def write_text(self, x, y, data):
+        if not len(data) or y >= self.rows: return
+        if x + len(data) > self.columns:
+            data = data[:self.columns - min(x, self.columns)]
+        self.tbuf[y][x:x+len(data)] = data
+    def write_glyph(self, x, y, glyph_name):
+        if x >= self.columns or y >= self.rows:
+            return 1
+        self.tbuf[y][x] = glyph_name
+        return 1
+    def write_graphics(self, x, y, data):
+        pass
